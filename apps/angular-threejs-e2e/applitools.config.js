902e80cedb38020bddc2fdc090bf5325ee26bcf3
Oshri <oshri.kdoshim@nextsilicon.com>
2021-11-16 20:20:17 +0200

    Delete applitools.config.js
    Signed-off-by: Oshri <oshri.kdoshim@nextsilicon.com>

--- apps/angular-threejs-e2e/applitools.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-	testConcurrency: 5,
-	apiKey: 'xddNaS103104nsWxYGS5BvINsP104xPNmcprwB99d6Jyd105KpPM110',
-	browser: [{ width: 1000, height: 600, name: 'chrome' }],
-	batchName: 'Demo Batch - Cypress',
-	showLogs: true,
-	serverUrl: 'https://eyes.applitools.com'
-};
